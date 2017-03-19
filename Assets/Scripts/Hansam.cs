19046439f0dfbfef1d3a49e250a83d48badde6e9
unknown <nwuser@GM2150092nemoto.da.iwasaki.ac.jp>
2017-03-19 17:01:39 +0900

    ～3月19日17時までの進捗～ 先ほどの15時40分までの進捗に加えて、ブスになった時の動き、ブスからの攻撃を受ける時のハンサムの硬直がほぼ完成 残りは当たった時のリアクション、通常の追尾を8方向追尾じゃなく、滑らかに追尾させたい

--- Assets/Scripts/Hansam.cs
+++ Assets/Scripts/Hansam.cs
@@ -6,26 +6,35 @@ using UnityEngine;
 /// ハンサムの移動やブスと当たった時のリアクション
 /// </summary>
 public class Hansam : MonoBehaviour {
-    Vector2 hansam_spd = new Vector2(0.5f,0.5f);    //ハンサムのスピード
+    float hansam_spd = 0.015f;    //ハンサムのスピード
     public Transform Player;  //ブス追尾のためにinspectorからブス格納
+    int LR = 0;
+    float rad;
+    Vector2 Posistion;
 
     public enum Hansam_status{//ハンサムの状態管理をenumで番号付け
         normal, //まだイケメン(0)
-        stay,   //AoS
+        stay,   //AoS(1)
         ugly    //ブサイクなう(2)
     }
 
-    public Hansam_status status;
+    public Hansam_status status;//ハンサムのステータス
 
 	// Use this for initialization
-	void Start () {
+	void OnEnable () {
+        LR = Random.Range(0, 2);    //左右どっちに掃けるかをランダムで決める;
         status = Hansam_status.normal;//ハンサムの初期値はやっぱりイケメン
-	}
+        rad = Mathf.Atan2(
+            Player.transform.position.y - transform.position.y,
+            Player.transform.position.x - transform.position.x);
+    }
 
 	
 	// Update is called once per frame
 	void Update () {
+        
         Move_Hansam();
+
 	}
 
 
@@ -35,47 +44,53 @@ public class Hansam : MonoBehaviour {
     /// </summary>
     void Move_Hansam()
     {
-        Vector2 Position = transform.position;
+       
         switch (status)
         {
+            
             case Hansam_status.normal://ハンサムどぇ～す
-
-
+               
 
                 break;
             case Hansam_status.stay://AoS(アタック・オブ・スタンド)中に範囲内のハンサムは「時は止まる」
-                return;
+                break;
 
             case Hansam_status.ugly://ブッさ
-                int LR = Random.Range(0,1);//左右どっちに掃けるかをランダムで決める
                 switch (LR)//L = 左(0),R = 右(1)
                 {
-                    case '0'://左に掃ける
-                        Position.x += hansam_spd.x * -1.5f;
+
+                    case 0://左に掃ける
+                        transform.position += new Vector3(hansam_spd * 10, 0, 0);
                         break;
-                    case '1'://右に掃ける
-                        Position.x += hansam_spd.x * 1.5f;
+                    case 1://右に掃ける
+                        transform.position -= new Vector3(hansam_spd * 10, 0, 0);
                         break;
                 }
                 break;
             default:
+                LR = 0;
                 break;
         }
     }
-    /// <summary>
+    /*/// <summary>
     /// 当たった時のリアクション
     /// </summary>
-    void OnTriggerEnter(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         
-        /*if ()
+        if ()
         {
             //ブスと当たった時にブス(大山)から硬直発動の関数を発動
+            OnHit();
         }
         //アタック・オブ・スタンド時に当たった時にブスになる(タイミングはAoS発動時にBOX判定でハンサム固定)
+        if(){
+           status = Hansam_status.stay
+        }
+        //ブスになる
         if ()
         {
             status = Hansam_status.ugly;
-        }*/
-    }
+        }
+    }*/
 }
