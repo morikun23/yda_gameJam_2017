bacc2542a590f7f7a43e97c33d3e2c4560986019
DA\nwuser <noppo.kappa@gmail.com>
2017-03-19 15:49:19 +0900

    15時はんまでの進捗

---
+++ Assets/Scripts/Power_Mater.cs
@@ -0,0 +1,20 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power_Mater : MonoBehaviour {
+    float time;
+    // Use this for initialization
+    void timerStart()
+    {
+        float time = 5f;
+        iTween.ScaleTo(this.gameObject, iTween.Hash("y", 0f, "time", time));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
+*/
---
+++ Assets/Scripts/Time_limit.cs
@@ -0,0 +1,27 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Time_limit : MonoBehaviour
+{
+
+    Text text;
+    GameManager gameManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        gameManager = GameObject.Find("GameManeger").GetComponent<Gamemaneger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int time = (int)gameManager.GetLimitTime();
+        text.text = time.ToString();
+    }
+}
+*/
