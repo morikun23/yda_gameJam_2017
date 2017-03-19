0bd8123d1ac645134d6e432caae5cffdc8907b13
DA\nwuser <noppo.kappa@gmail.com>
2017-03-19 19:04:27 +0900

    3/19（日）19：04までの進捗

---
+++ Assets/Scripts/title_flash.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class title_flash : MonoBehaviour
+{
+    private float nextTime;
+    public float interval = 1f;   // 点滅周期
+
+    SpriteRenderer sp;
+
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+        nextTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextTime)
+        {
+            sp.enabled = !sp.enabled;
+            nextTime += interval;
+        }
+    }
+}
