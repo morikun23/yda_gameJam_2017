0a97811bf8e7734fce7d52df363b9b17f4bedc04
MoritaSuguru <morisugood@gmail.com>
2017-03-20 12:57:43 +0900

    【SoundListの修正】

--- Assets/Scripts/Audio/SoundList.cs
+++ Assets/Scripts/Audio/SoundList.cs
@@ -3,20 +3,11 @@
 /// ※フォルダに入っている名前と一致させること
 /// </summary>
 public enum SE {
-<<<<<<< HEAD
     Attack,
     BusuHenka,
     Charge,
     Damage,
     Select
-=======
-    attack,
-    busuhenka,
-    charge,
-    damage,
-    select,
-    bomb
->>>>>>> origin/Oyama
 }
 
 /// <summary>
